a40bacc42e1645ef2ff8d99604ed50c68ccaf6ec
Rafael Quadros <contatorafaelquadros@gmail.com>
2023-01-12 13:23:12 -0300

    fix: mocks

--- src/__tests__/integration/techs/techsRoutes.test.ts
+++ src/__tests__/integration/techs/techsRoutes.test.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 import AppDataSource from "../../../data-source";
 import request from "supertest";
 import app from "../../../app";
+import { mockedUser, mockedUserLogin } from "../../mocks";
 
 describe("/technologies", () => {
   let connection: DataSource;
@@ -24,10 +25,23 @@ describe("/technologies", () => {
   });
 
   test("GET /technologies -  Must be able to list technologies", async () => {
-    const response = await request(app).get("/technologies");
+    await request(app).post("/users").send(mockedUser);
+    const userLoginResponse = await request(app)
+      .post("/login")
+      .send(mockedUserLogin);
+    const response = await request(app)
+      .get("/technologies")
+      .set("Authorization", `Bearer ${userLoginResponse.body.token}`);
 
     expect(response.body).toHaveLength(19);
     expect(response.body[0]).toHaveProperty("id");
     expect(response.body[0]).toHaveProperty("name");
+  });
+
+  test("GET /technologies -  should not be able to list technologies without authentication", async () => {
+    const response = await request(app).get("/technologies");
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(401);
   });
 });
