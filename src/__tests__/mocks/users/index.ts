b4772f4a27f899325fe51480a9215ca4b102ddda
brunobbittencourt <bbittencourt@outlook.com.br>
2023-01-16 20:12:36 -0300

    feat: user to projects tests

--- src/__tests__/mocks/users/index.ts
+++ src/__tests__/mocks/users/index.ts
@@ -1,5 +1,6 @@
 import { ILogin } from "../../../interfaces/login";
-import { iUserRequest } from "../../../interfaces/users";
+import { IProjectRequest } from "../../../interfaces/projects";
+import { iUser, iUserRequest } from "../../../interfaces/users";
 
 export const mockedAdmin: iUserRequest = {
   name: "Rafael Quadros",
@@ -31,4 +32,31 @@ export const mockedAdminLogin: ILogin = {
 export const mockedUserLogin: ILogin = {
   email: "bruno@mail.com",
   password: "123456",
+};
+
+export const mockedUserToProjects: IProjectRequest = {
+  title: "novo projeto",
+  description: "projeto front-end",
+  projectsPicture: "string",
+  status: "pending",
+};
+
+export const mockedDeletedUser: iUserRequest = {
+  name: "bruno",
+  email: "deleted@mail.com",
+  isAdm: false,
+  password: "123456",
+  github: "www.github.com",
+  linkedin: "www.linkedin.com",
+  profilePicture:
+    "https://t.ctcdn.com.br/IVlt3nVuXYDVX4vyjzgborR84H0=/400x400/smart/i490793.jpeg",
+};
+
+export const mockedDeletedUserLogin: ILogin = {
+  email: "deleted@mail.com",
+  password: "123456",
+};
+
+export const mockedProjectId = {
+  id: undefined,
 };
